14489d3b035bd89becadca96078124aa8e1e46c7
kanghuawu <kang-hua.wu@sjsu.edu>
2017-09-03 23:51:28 -0700

    hello world app

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Kang-Hua Wu on 9/1/17.
+//  Copyright © 2017 Kang-Hua Wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
